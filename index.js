94f45aa3f41b7ae435c5b7f6287e7ece60460f86
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-06-08 16:43:58 +0300

    commit tree 0649956cfbe4807a5af161fa1460d7c7e70c7d82

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+//1. customerName
+//===================
+var customerName = "bob";
+
+//2. upperCaseCustomerName
+//==========================
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+//3. setBestCustomer
+//===================
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+//4. overwriteBestCustomer
+//==========================
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+//5. leastFavoriteCustomer && changeLeastFavoriteCustomer
+//========================================================
+const leastFavoriteCustomer = "joffrey";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "The Joker";
+}
